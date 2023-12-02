98123964f14941a9b99db3b14b415d4f5d0b8fb6
Andrew Stanecek <andstanecek@gmail.com>
2023-12-02 12:30:06 -0700

    application is dockerized and running with nginx

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,7 +1,3 @@
-<script setup>
-import Books from './Books.vue'
-</script>
-
 <template>
     <section>
       <div class="text-center bg-red text-black mx-0">
@@ -14,12 +10,10 @@ import Books from './Books.vue'
             <p >Mr. Gottlieb brings his years of experience as a trial attorney to his books, taking you into the courtroom where you experience the planning, drama, excitement, mystery, and the twists and turns as cases unfold.  You’ll wonder if they are real cases being revisited, or fictional.The how what, when and why of the mystery is for you to figure out before the final testimony. Are you up to the challenge?</p>
             <p>In writing these novels, Mr. Gottlieb relives his years in the court room proving, “You can take the attorney out of the courtroom, but you can’t take the courtroom out of the attorney!”</p>
             <div class="mt-4">
-              <a class="btn btn-light home-button mx-1" href="/books" >Books</a>
-              <!-- <a class="btn home-button mx-1" href="/about" >About</a> -->
+              <router-link class="btn btn-light home-button mx-1" to="/books">Books</router-link>
             </div>
           </div>
         </div>
-        <Books/>
       </div>
     </section>
 </template>
